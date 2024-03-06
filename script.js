fdb61922fd613b6a5d84aa3f950b83e6c0fd15fa
Abizar <abizararrifai@gmail.com>
2024-03-07 00:00:59 +0700

    memperbarui html css dan js

--- script.js
+++ script.js
@@ -34,3 +34,32 @@ window.onscroll = () => {
   menuIcon.classList.remove("bx-x");
   navbar.classList.remove("active");
 };
+
+// typed JS
+const typed = new Typed(".text", {
+  strings: ["Fullstack Developer."],
+  typeSpeed: 70,
+  backSpeed: 70,
+  backDelay: 1000,
+  loop: true,
+});
+
+// dark light mode
+let darkModeIcon = document.querySelector("#darkMode-icon");
+darkModeIcon.onclick = () => {
+  darkModeIcon.classList.toggle("bx-sun");
+  document.body.classList.toggle("dark-mode");
+};
+
+// scroll reveal
+ScrollReveal({
+  // reset: true,
+  distance: "80px",
+  duration: 2000,
+  delay: 200,
+});
+
+ScrollReveal().reveal(".home-content, .heading", { origin: "top" });
+ScrollReveal().reveal(".home-img img, .portfolio-box, .contact form", { origin: "bottom" });
+ScrollReveal().reveal(".home-content h1, .about-img img", { origin: "left" });
+ScrollReveal().reveal(".home-content h3, .home-content p, .about-content", { origin: "right" });
