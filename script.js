a41cafba5a75ee52debd1d0445edb8c648653d6b
Abizar <abizararrifai@gmail.com>
2024-03-07 23:24:47 +0700

    menambahkan fitur swiper pada section sertifikat

--- script.js
+++ script.js
@@ -35,6 +35,22 @@ window.onscroll = () => {
   navbar.classList.remove("active");
 };
 
+// swiper
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 50,
+  loop: true,
+  grabCursor: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
 // typed JS
 const typed = new Typed(".text", {
   strings: ["Fullstack Developer."],
@@ -60,7 +76,7 @@ ScrollReveal({
 });
 
 ScrollReveal().reveal(".home-content, .heading", { origin: "top" });
-ScrollReveal().reveal(".home-img img, .portfolio-box, .contact form", { origin: "bottom" });
+ScrollReveal().reveal(".home-img img, .portfolio-box, .certi-wrapper, .contact form", { origin: "bottom" });
 ScrollReveal().reveal(".home-content h1, .about-img img", { origin: "left" });
 ScrollReveal().reveal(".home-content h3, .home-content p, .about-content", { origin: "right" });
 
